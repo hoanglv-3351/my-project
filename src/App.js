3473fa477113bd3f2a4afa05fe20e4b207480b5d
hoanglv-0162 <hoangh2kq@gmail.com>
2022-07-03 19:56:27 +0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+        {/* CSS */}
+        {/* https://boxicons.com/usage#import-css */}
+        {/* <Content /> */}
+        {/* <Footer /> */}
     </div>
   );
 }
